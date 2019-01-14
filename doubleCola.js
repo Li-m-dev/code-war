9d369daf1beb04acdfe4b379838dc3c947f00ba5
Li Swafford <liswafford@yahoo.com>
2019-01-13 20:48:10 -0600

    start

---
+++ doubleCola.js
@@ -0,0 +1,12 @@
+// Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on.
+
+function whoIsNext(names, r){
+  //your code here
+}
+
+
+
+names = ["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"];
+r = 1;
+res = "Sheldon";
+Test.assertEquals(whoIsNext(names, r), res);
--- toyProblem.js
+++ toyProblem.js
@@ -12,6 +12,19 @@ function toWeirdCase(string) {
     .join(" ");
 }
 
+function toWeirdCase(string) {
+  return string.split(" ").map(word =>
+    word
+      .split("")
+      .map(
+        (letter, i) =>
+          i % 2 === 0 ? letter.toUpperCase() : letter.toLowerCase()
+      )
+      .join("")
+      .join(" ")
+  );
+}
+
 function tea42(input) {
   let array = input.toString().split("");
   for (let i = 0; i < array.length; i++) {
